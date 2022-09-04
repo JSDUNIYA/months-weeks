ec6462ff6a5fbae54d4ba23a9b5cfe03c3769258
jsduniya <jsduniya747@gmail.com>
2022-09-04 13:23:02 +0530

    init commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const months = {
+    January, February, March, April, May, June, July, August, September, October, November, December
+}
+
+const weeks = {
+    Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+}
+
+exports.months = months;
+exports.weeks = weeks
